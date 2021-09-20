7c90388e8f20ea09f48cefb74ceea1aba1391ba6
synplatina <rizalnoerdin13@gmail.com>
2021-09-20 02:50:50 +0000

    platina: Add MIUI Camera

--- aosp_platina.mk
+++ aosp_platina.mk
@@ -21,6 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
 # Inherit from platina device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
